2c6b4792ca533879b2395bff16a68831f34f3692
daosyn <brandon@daosyn.info>
2023-12-12 13:06:31 -0600

    add 3d models, deploy to gh-pages

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 "use client";
-import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import styles from "./page.module.css";
-import * as THREE from "three";
 
 export default function Home() {
   const [time, setTime] = useState(null);
@@ -26,84 +26,35 @@ export default function Home() {
     return h + ":" + m + ":" + s;
   }
 
-  const images = [
-    "/bart.jpg",
-    "/donk.png",
-    "/himalsmall.gif",
-    "/poorguy.jpg",
-    "/bart.jpg",
-    "/donk.png",
-    "/himalsmall.gif",
-    "/poorguy.jpg",
-    "/bart.jpg",
-    "/donk.png",
-    "/himalsmall.gif",
-    "/poorguy.jpg",
-    "/bart.jpg",
-    "/donk.png",
-    "/himalsmall.gif",
-    "/poorguy.jpg",
-    "/bart.jpg",
-    "/donk.png",
-    "/himalsmall.gif",
-    "/poorguy.jpg",
-  ];
+  const images = ["/bart.jpg", "/donk.png", "/himalsmall.gif", "/poorguy.jpg"];
+  const models = ["isaac", "strongbad"];
 
-  function renderCube() {
+  function renderModel(name, idx) {
+    const gltf = useLoader(GLTFLoader, `/${name}/scene.gltf`);
     return (
-      <div className={styles.wrap}>
-        <div className={styles.cube}>
-          <div className={styles.front}>front</div>
-          <div className={styles.back}>back</div>
-          <div className={styles.top}>top</div>
-          <div className={styles.bottom}>bottom</div>
-          <div className={styles.left}>left</div>
-          <div className={styles.right}>right</div>
-        </div>
-      </div>
+      <group position={[idx, idx, idx]}>
+        <primitive object={gltf.scene} />
+      </group>
     );
   }
-
-  // const scene = new THREE.Scene();
-  // const camera = new THREE.PerspectiveCamera(
-  //   75,
-  //   500, //window.innerWidth / window.innerHeight,
-  //   0.1,
-  //   1000
-  // );
-
-  // const renderer = new THREE.WebGLRenderer();
-  // renderer.setSize(500); //window.innerWidth, window.innerHeight);
-  // document.body.appendChild(renderer.domElement);
-
-  // const geometry = new THREE.BoxGeometry(1, 1, 1);
-  // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  // const cube = new THREE.Mesh(geometry, material);
-  // scene.add(cube);
-
-  // camera.position.z = 5;
-
-  // function animate() {
-  //   requestAnimationFrame(animate);
-
-  //   cube.rotation.x += 0.01;
-  //   cube.rotation.y += 0.01;
-
-  //   renderer.render(scene, camera);
-  // }
-  // animate();
 
   return (
     <main className={styles.main}>
       <div className={styles.time}>{time}</div>
-      <div className={styles.grid}>
+      {/* <div className={styles.grid}>
         {images.map((i) => (
           <div className={styles.image} key={i}>
             <Image src={i} height="150" width="150" alt={i} />
           </div>
         ))}
+      </div> */}
+      <div className={styles.grid}>
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="white" position={[1, 1, 5]} />
+          <mesh>{models.map((name, idx) => renderModel(name, idx))}</mesh>
+        </Canvas>
       </div>
-      {/* {renderCube()} */}
     </main>
   );
 }
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#! bash
+
+STATUS="$(git status)"
+
+if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
+then
+    sed -i "" '/out/d' ./.gitignore
+    git add .
+    git commit -m "deploy '$(git log -1 --pretty=format:%B)'"
+    git push origin `git subtree split --prefix dist main`:gh-pages --force
+    git reset HEAD~
+    git checkout .gitignore
+else
+    echo "need clean working directory to publish"
+fi
