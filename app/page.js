3eb1595643458114f54a41ca61c4ae24716ea89a
daosyn <brandon@daosyn.info>
2023-12-12 09:48:21 -0600

    add three.js, bg.gif

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
+import * as THREE from "three";
 
 export default function Home() {
   const [time, setTime] = useState(null);
@@ -27,14 +28,59 @@ export default function Home() {
 
   const images = ["/bart.jpg", "/donk.png", "/himalsmall.gif", "poorguy.jpg"];
 
+  function renderCube() {
+    return (
+      <div className={styles.wrap}>
+        <div className={styles.cube}>
+          <div className={styles.front}>front</div>
+          <div className={styles.back}>back</div>
+          <div className={styles.top}>top</div>
+          <div className={styles.bottom}>bottom</div>
+          <div className={styles.left}>left</div>
+          <div className={styles.right}>right</div>
+        </div>
+      </div>
+    );
+  }
+
+  // const scene = new THREE.Scene();
+  // const camera = new THREE.PerspectiveCamera(
+  //   75,
+  //   500, //window.innerWidth / window.innerHeight,
+  //   0.1,
+  //   1000
+  // );
+
+  // const renderer = new THREE.WebGLRenderer();
+  // renderer.setSize(500); //window.innerWidth, window.innerHeight);
+  // document.body.appendChild(renderer.domElement);
+
+  // const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  // const cube = new THREE.Mesh(geometry, material);
+  // scene.add(cube);
+
+  // camera.position.z = 5;
+
+  // function animate() {
+  //   requestAnimationFrame(animate);
+
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
+
+  //   renderer.render(scene, camera);
+  // }
+  // animate();
+
   return (
     <main className={styles.main}>
       <div className={styles.time}>{time}</div>
       {images.map((i) => (
-        <div className={styles.image}>
+        <div className={styles.image} key={i}>
           <Image src={i} height="150" width="150" alt={i} />
         </div>
       ))}
+      {/* {renderCube()} */}
     </main>
   );
 }
