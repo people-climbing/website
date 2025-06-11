7a7f574950b91a62c1abbf7140dfb817d86d8647
daosyn <brandon@daosyn.info>
2025-06-10 22:36:47 -0500

    try to unfix sed

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ STATUS="$(git status)"
 
 if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
-    sed -i'' '/out/d' ./.gitignore
+    sed -i "" '/out/d' ./.gitignore
     git add .
     git commit -m "deploy '$(git log -1 --pretty=format:%B)'"
     git push origin `git subtree split --prefix out main`:gh-pages --force
