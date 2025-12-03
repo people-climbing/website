7b34043bd7a6eaeaecf9b52f1568d89b16547978
daosyn <brandon@daosyn.info>
2025-12-03 15:47:12 -0600

    add lint config, fix lint

--- src/app/thanks/page.jsx
+++ src/app/thanks/page.jsx
@@ -8,7 +8,7 @@ export default function ThanksPage() {
     <div className={styles.container}>
       <Image src={himal} height={100} width={100} alt="himal dancing" />
       <p>
-        hey! thanks for purchasing! you'll receive an email and all that shit
+      {"hey! thanks for purchasing! you'll receive an email and all that shit"}
       </p>
     </div>
   );
